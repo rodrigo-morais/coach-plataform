2a9aa503244d031193a8e535c9df10b550545c4a
Vinicius Fernandes <viniciusffj@gmail.com>
2016-04-01 18:02:55 -0300

    Adds functionality to save a coach

--- src/Coaches/Edit/Models.elm
+++ src/Coaches/Edit/Models.elm
@@ -5,28 +5,26 @@ type alias CoachId = Int
 
 
 type alias Coach =
-    {
-        id: CoachId,
-        spots: Int,
-        name: String,
-        mentor: Bool,
-        coach: Bool,
-        capabilities: String,
-        description: String
-    }
+  { id: CoachId
+  , spots: Int
+  , name: String
+  , mentor: Bool
+  , coach: Bool
+  , capabilities: String
+  , description: String
+  }
 
 
 initialCoach : Coach
 initialCoach =
-    {
-        id = 0,
-        spots = 0,
-        name = "",
-        mentor = False,
-        coach = False,
-        capabilities = "",
-        description = ""
-    }
+  { id = 0
+  , spots = 0
+  , name = ""
+  , mentor = False
+  , coach = False
+  , capabilities = ""
+  , description = ""
+  }
 
 type MessageType =
   Blank
@@ -35,29 +33,25 @@ type MessageType =
 
 
 type alias Message =
-  {
-    messageType : MessageType,
-    text : String
+  { messageType : MessageType
+  , text : String
   }
 
 
 initialMessage : Message
 initialMessage =
-  {
-    messageType = Blank,
-    text = ""
+  { messageType = Blank
+  , text = ""
   }
 
 
 type alias ViewModel =
-  {
-    coach : Coach,
-    message : Message
+  { coach : Coach
+  , message : Message
   }
 
 
 initialViewModel =
-  {
-    coach = initialCoach,
-    message = initialMessage
+  { coach = initialCoach
+  , message = initialMessage
   }
--- src/Coaches/Edit/Update.elm
+++ src/Coaches/Edit/Update.elm
@@ -6,22 +6,66 @@ import Effects exposing (Effects)
 
 import Coaches.Edit.Models exposing (..)
 import Coaches.Edit.Actions exposing (..)
+import Coaches.Edit.Effects exposing (..)
 
 
 update : Action -> ViewModel -> (ViewModel, Effects Action)
-update action model =
-    case action of
-        NoOp -> (model, Effects.none)
+update action model = 
+  let 
+    coach = Debug.log "coach" model.coach
 
-        SaveCoach ->
-          let
-            updateMessage message =
-              { message | text = "Error when saving coach.", messageType = Error }
+    updateCoach newCoach = 
+      ({model | coach = newCoach}, Effects.none) 
+  in
+    case action of 
+      NoOp -> (model, Effects.none)
+
+      Save -> (model, saveCoaches coach)
+
+      SaveDone result -> 
+        case result of
+          Ok coach ->
+            let
+              updatedViewModel =
+                { coach = model.coach
+                , message = successMessage 
+                }
+            in
+              (updatedViewModel, Effects.none)
+
+          Err error ->
+            ({model | message = errorMessage}, Effects.none)
+
+      UpdateSpotsIncrease ->
+        updateCoach({coach | spots = coach.spots + 1})
+
+      UpdateSpotsDecrease ->
+        updateCoach({coach | spots = coach.spots - 1})
+
+      UpdateName newName -> 
+        updateCoach({coach | name = newName})
+
+      UpdateMentor value -> 
+        updateCoach({coach | mentor = value})
+
+      UpdateCoach value -> 
+        updateCoach({coach | coach = value})
+
+      UpdateCapabilities value -> 
+        updateCoach({coach | capabilities = value})
+
+      UpdateDescription value -> 
+        updateCoach({coach | description = value})
 
 
-            updatedViewModel =
-              { coach = model.coach, message = (updateMessage model.message) }
+errorMessage : Message
+errorMessage =
+  { text = "Error when saving coach."
+  , messageType = Error 
+  }
 
-
-          in
-            (updatedViewModel, Effects.none)
+successMessage : Message
+successMessage =
+  { text = "New coach created."
+  , messageType = Success
+  }
