cc371528fdd27375ac43f3445862c0f9245a7460
Vinicius Fernandes <viniciusffj@gmail.com>
2016-04-11 23:26:22 -0300

    Adding style to search page

--- src/Coaches/Search/View.elm
+++ src/Coaches/Search/View.elm
@@ -11,48 +11,66 @@ import Html.Events exposing (..)
 
 view : Signal.Address Action -> ViewModel -> Html.Html
 view address model = 
-  div 
-    []
-    [ searchFields address model 
-    , coaches address model.coaches
+  div
+    [ class "clearfix" ]
+    [ div
+        [ class "col-8 mx-auto mt2" ]
+        [ div
+            []
+            [searchFields address model]
+        , coaches address model.coaches
+        ]
     ]
-
+  
 
 searchFields : Signal.Address Action -> ViewModel -> Html.Html
 searchFields address model =
   div
-    []
-    [ input 
-        [ value model.capabilities
-        , placeholder "capabilities"
-        , CoachesView.onTextChange address UpdateCapabilities
+    [ class "clearfix"]
+    [ div
+        [ class "col col-9"]
+        [ input 
+          [ class "input col-12 col py1"
+          , value model.capabilities
+          , placeholder "Search by capabilities"
+          , CoachesView.onTextChange address UpdateCapabilities
+          ]
+          []
         ]
-        []
-    , searchButton address
+    , div
+        [ class "col col-3 px2" ]
+        [ searchButton address ]
     ]
 
 
 searchButton : Signal.Address Action -> Html.Html
 searchButton address =
-  button [onClick address SearchCoaches] [text "Search"]
+  button 
+    [ onClick address SearchCoaches
+    , class "btn rounded white bg-black"
+    ] 
+    [ text "Search"]
 
 
 coaches : Signal.Address Action -> Coaches -> Html.Html
 coaches address coaches =
   let
     coachRow coach =
-      tr
-        []
-        [ td [] [text coach.name]
-        , td [] [text (toString coach.spots)]
-        , td [] [text (toString coach.mentor)]
-        , td [] [text (toString coach.coach)]
-        , td [] [text coach.capabilities]
-        , td [] [text coach.description]
+      div
+        [class "overflow-hidden border rounded mt2"]
+        [ div
+            [class "p2 bold white bg-black"]
+            [text coach.name]
+        , div
+            [class "p2"]
+            [text coach.capabilities]
+        , div
+            [class "p2"]
+            [text coach.description]
         ]
     coachList = List.map coachRow coaches 
   in
-    table
-      []
+    div
+      [ class "mt1"]
       coachList
 
