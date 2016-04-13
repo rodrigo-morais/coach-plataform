ee3fed621f04a3a6250cc52ac48ab3ee0f91fb8f
Rodrigo Morais <morais.rm@gmail.com>
2016-04-12 23:52:45 -0300

    Adding routes to navigation bar.

--- src/Navigation/View.elm
+++ src/Navigation/View.elm
@@ -2,49 +2,86 @@ module Navigation.View (..) where
 
 
 import Html exposing (..)
-import Html.Attributes exposing (class, style)
+import Html.Attributes exposing (class, style, href)
 
 
-view : Html.Html
-view =
-  nav
+import Models exposing (..)
 
 
-nav : Html.Html
-nav =
+view : AppModel -> Html.Html
+view model =
+  nav model
+
+
+nav : AppModel -> Html.Html
+nav model =
   div 
     [ class "clearfix mb2 white bg-black", style [("min-height", "3.5em")]  ]
     [ div 
         [ class "left p2 col col-10" ]
-        [ listCoaches
-        , newCoach
-        , editCoach
+        [ listCoaches model
+        , newCoach model
         ]
     ]
 
 
-listCoaches : Html.Html
-listCoaches =
-  div [ class "col col-1 border border-white rounded p1 px2 mr2" ]
-      [ i [ class "fa fa-users mr1" ]
-          [ ]
-      , text "List"
+listCoaches : AppModel -> Html.Html
+listCoaches model =
+  let
+    (stl, cls) =
+      styles model "search"
+
+  in
+    a [ href "#/coaches/search", class "text-decoration-none", style [stl] ]
+      [ div [ class cls ]
+            [ i [ class "fa fa-users ml1 mr1" ]
+                [ ]
+            , text "List"
+            ]
       ]
 
 
-newCoach : Html.Html
-newCoach =
-  div [ class "col col-1 border border-white rounded p1 mr2" ]
-      [ i [ class "fa fa-user-plus mr1"]
-          [ ]
-      , text "New"
+newCoach : AppModel -> Html.Html
+newCoach model =
+  let
+    (stl, cls) =
+      styles model "new"
+
+  in
+    a [ href "#/coaches/new", class "text-decoration-none", style [stl] ]
+      [ div [ class cls ]
+            [ i [ class "fa fa-user-plus ml1 mr1 normal", style [("width", "1em")]]
+                [ ]
+            , text "New"
+            ]
       ]
 
 
-editCoach : Html.Html
-editCoach =
-  div [ class "col col-1 border border-white rounded p1 px2 mr2" ]
-      [ i [ class "fa fa-pencil-square-o mr1"]
-          [ ]
-      , text "Edit"
-      ]
+styles : AppModel -> String -> ((String, String), String)
+styles model route =
+  let
+    routeModel =
+      model.routeModel
+
+    location =
+      routeModel.location
+
+    path =
+      location.path
+
+    maybeList =
+      List.foldl (::) [] path
+      |> List.head
+
+    (stl, cls) =
+      case maybeList of
+        Just lst ->
+          if lst == route then
+            (("color", "#0074d9"), "col col-1 border border-blue rounded p1 mr2")
+          else
+            (("color", "#ffffff"), "col col-1 border border-white rounded p1 mr2")
+
+        Nothing ->
+          (("color", "#ffffff"), "col col-1 border border-white rounded p1 mr2")
+  in
+    (stl, cls)
