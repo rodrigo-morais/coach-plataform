788a13c1f7962db5beb531502177b95379610658
Rodrigo Morais <morais.rm@gmail.com>
2016-04-25 22:49:21 -0300

    Configuring the e2e test.

--- tests/e2e/create-coach.js
+++ tests/e2e/create-coach.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< 33a9ce2f48a586e158ab175fc3778fa35acb4704
   'List all coaches and mentors' : function (browser) {
     browser
       .url('http://192.168.99.100')
@@ -6,6 +7,17 @@ module.exports = {
       .click('button[id=searchButton]')
       .pause(1000)
       .assert.containsText('#main', 'Kathryn Watson')
+=======
+  'Looking for Thoughtworks on Google' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'thoughtworks')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'https://www.thoughtworks.com')
+>>>>>>> Configuring the e2e test.
       .end();
   }
 };
