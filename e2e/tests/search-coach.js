1519adb4584fa9830248b94a2943e601e50517d3
Rodrigo Morais <morais.rm@gmail.com>
2016-05-22 08:32:27 -0300

    Adding test to add coach.

--- e2e/tests/search-coach.js
+++ e2e/tests/search-coach.js
@@ -1,11 +1,29 @@
 module.exports = {
-  'List all coaches and mentors' : function (browser) {
+  'Add a new coach,' : function (browser) {
+    browser
+      .url('http://localhost:5000')
+      .waitForElementVisible('a[id=newCoach]', 1000)
+      .url('http://localhost:5000/#/coaches/new')
+      .waitForElementVisible('a[id=increaseSpot]', 1000)
+      .click('a[id=increaseSpot]')
+      .click('a[id=increaseSpot]')
+      .click('a[id=increaseSpot]')
+      .setValue('input[id=coachName]', 'Rodrigo Morais')
+      .click('input[id=coachCheck]')
+      .setValue('input[id=capabilities]', 'Elm e2e Nightwatch')
+      .setValue('textarea[id=description]', 'Software Engineer')
+      .click('button[id=saveCoach]')
+      .pause(5000)
+      .assert.containsText('#main', 'New coach created with success.');
+  },
+
+  'Search added coach' : function (browser) {
     browser
       .url('http://localhost:5000')
       .waitForElementVisible('button[id=searchButton]', 1000)
       .click('button[id=searchButton]')
       .pause(1000)
-      .assert.containsText('#main', 'Kathryn Watson')
+      .assert.containsText('#main', 'Rodrigo Morais')
       .end();
   }
 };
